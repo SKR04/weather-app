c635553e2e54a6a0e1953c1d05e0d357e37d19d9
SKR04 <s.k.rajasekar2000@gmail.com>
2024-07-05 14:07:33 +0530

    modified:   src/components/CurrentWeather.tsx

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -18,20 +18,20 @@ const CurrentWeather: React.FC<Props> = ({ forecast, isCelsius }) => {
         <span className="font-normal">{forecast ? forecast.country : ''}</span>
       </div>
       <div className="flex gap-3 items-center justify-start">
-        <div className="w-[120px] h-[100px]">
+        <div className="md:w-[120px] md:h-[100px] min-[320px]:w-[90px] min-[320px]:h-[80px]">
           <img
             src={getIcon(today.weather[0].id)}
             className="w-full h-full object-cover"
             alt="Weather Icon"
           />
         </div>
-        <div className="w-[150px]">
-          <h1 className="text-6xl font-medium text-gray-700">
+        <div className="md:w-[150px] w-[100px]">
+          <h1 className="text-5xl md:text-6xl font-medium text-gray-700">
             <Degree temp={Math.round(today ? today.main.temp : 0)} deg={isCelsius} />
           </h1>
         </div>
-        <div className="w-[160px] ml-3">
-          <h2 className="text-lg">
+        <div className="md:w-[160px] md:ml-3 ">
+          <h2 className="md:text-lg text-base">
             {today
               ? today.weather[0].description.charAt(0).toUpperCase() +
                 today.weather[0].description.slice(1)
@@ -43,7 +43,7 @@ const CurrentWeather: React.FC<Props> = ({ forecast, isCelsius }) => {
           </h2>
         </div>
       </div>
-      <p className="text-base font-semibold pl-3 pb-5">
+      <p className="md:text-base font-semibold pl-3 pb-5 text-sm">
         {`${getPop(today ? today.pop : 0)} of precipitation, clouds at ${
           today ? today.clouds.all : 0
         }%`}
